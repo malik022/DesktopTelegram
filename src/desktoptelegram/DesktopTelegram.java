da97fb57a02ffc4546da27fcb3762b879b518235
Dielson Sales <dielsonscarvalho@gmail.com>
2014-04-12 20:31:06 -0300

    List with icons

--- src/desktoptelegram/DesktopTelegram.java
+++ src/desktoptelegram/DesktopTelegram.java
@@ -5,13 +5,15 @@ import java.awt.Color;
 import java.awt.Component;
 import java.util.ArrayList;
 import java.util.List;
-import javax.swing.DefaultListModel;
 import javax.swing.ImageIcon;
+import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JList;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.ListCellRenderer;
 import javax.swing.ListSelectionModel;
+import javax.swing.SwingConstants;
 
 /**
  * The main application window.
@@ -20,17 +22,13 @@ import javax.swing.ListSelectionModel;
 public class DesktopTelegram extends javax.swing.JFrame {
 
     public DesktopTelegram() {
-        // init components
         setTitle("Desktop Telegram");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        
+
         messageItems = new ArrayList<MessageItem>();
         messageItems.add(new MessageItem("Contato 1", "assets/contactIconSmall.png"));
         messageItems.add(new MessageItem("Contato 2", "assets/contactIconSmall.png"));
-
-//        modelList = new DefaultListModel<MessageItem>();
-//        modelList.addElement(new MessageItem("Mensagem 1"));
-//        modelList.addElement(new MessageItem("Mensagem 2"));
+        messageItems.add(new MessageItem("Contato 3", "assets/contactIconSmall.png"));
 
         messagesList = new JList(messageItems.toArray());
         messagesList.setCellRenderer(new MessageItemRender());
@@ -43,56 +41,63 @@ public class DesktopTelegram extends javax.swing.JFrame {
 
     public static void main(String[] args) {
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
+
             @Override
             public void run() {
                 new DesktopTelegram().setVisible(true);
             }
         });
     }
-
-    // variables
-    DefaultListModel<MessageItem> modelList;
+    // variables ---------------------------------------------------------------
     JList messagesList;
-    List<MessageItem> messageItems;
-    
-    private class MessageItem extends javax.swing.JPanel {
+    private List<MessageItem> messageItems;
+}
 
-        public MessageItem(String contactName, String iconPath) {
-            this.contactName = contactName;
-            this.iconPath = iconPath;
-        }
-        public String getContactName() {
-            return contactName;
-        }
-        public String getIconPath() {
-            return iconPath;
-        }
-        String contactName;
-        String iconPath;
+class MessageItem extends javax.swing.JPanel {
+
+    public MessageItem(String contactName, String iconPath) {
+        this.contactName = contactName;
+        this.iconPath = iconPath;
     }
-    
-    private class MessageItemRender extends JLabel implements ListCellRenderer {
-        private final Color HIGHLIGHT_COLOR = new Color(0, 0, 128);
 
-        public MessageItemRender() {
-            setOpaque(true);
-        }
+    public String getContactName() {
+        return contactName;
+    }
 
-        @Override
-        public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
-            MessageItem item = (MessageItem) value;
-            setText(item.getContactName());
-            ImageIcon icon = new ImageIcon(item.getIconPath());
-            setIcon(icon);
-            if (isSelected) {
-                setBackground(Color.blue);
-                setForeground(Color.white);
-            } else {
-                setBackground(Color.white);
-                setForeground(Color.black);
-            }
-            return this;
+    public String getIconPath() {
+        return iconPath;
+    }
+    String contactName;
+    String iconPath;
+}
+
+class MessageItemRender extends JPanel implements ListCellRenderer {
+    JLabel itemTitle;
+    JLabel contactImage;
+
+    public MessageItemRender() {
+        System.out.println("Creating a new MessageItemRenderer()");
+        setLayout(new BorderLayout());
+        itemTitle = new JLabel();
+        contactImage = new JLabel();
+        add(contactImage, BorderLayout.WEST);
+        add(itemTitle, BorderLayout.EAST);
+    }
+
+    @Override
+    public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
+        MessageItem item = (MessageItem) value;
+
+        itemTitle.setText(item.getContactName());
+//            ImageIcon icon = new ImageIcon(item.getIconPath());
+        contactImage.setIcon(new ImageIcon(item.getIconPath()));
+        if (isSelected) {
+            setBackground(Color.yellow);
+//                setForeground(Color.white);
+        } else {
+            setBackground(Color.white);
+//                setForeground(Color.black);
         }
-        
+        return this;
     }
 }
