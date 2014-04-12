e935182f110a786910353987455c6135fb57134e
Dielson Sales <dielsonscarvalho@gmail.com>
2014-04-12 17:54:41 -0300

    Added running function

--- src/desktoptelegram/DesktopTelegram.java
+++ src/desktoptelegram/DesktopTelegram.java
@@ -12,11 +12,19 @@ import javax.swing.JFrame;
  */
 public class DesktopTelegram extends JFrame {
 
+    public DesktopTelegram() {
+        // init components
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new DesktopTelegram().setVisible(true);
+            }
+        });
     }
 }
